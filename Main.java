df34288166b72930334651ee2939f3eb01d8d405
QuincyBraxton1 <quincy.braxton@hgs.hiddengeniusproject.org>
2021-11-13 02:50:12 +0000

    Made variables program

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String name = "Quincy Braxton";
+    String timeManagment = "Taking breaks in between long sessions of work to keep myself refreshed and not burnt out";
+    String personalCommitment = "getting better at prioritizing the right things";
+    String KPIMetrics = "number of customers, customer feedback and reoccuring customers";
+
+    System.out.println("Hello my name is " + name + ", and the time management strategy that I would like to be held accountable\n" + "to is " + timeManagment + ". I will personally commit to " + personalCommitment + ". " + "My business main KPI is/are " + KPIMetrics + ".");
+
+
+
+
+  }
+}
